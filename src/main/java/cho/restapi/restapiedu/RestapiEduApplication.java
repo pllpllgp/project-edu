7ef380bacaebaddc5b9fd12f9d37f5e35cf0643e
Cho <pllpllgp@gmail.com>
2020-03-16 22:42:04 +0900

    project create

---
+++ src/main/java/cho/restapi/restapiedu/RestapiEduApplication.java
@@ -0,0 +1,13 @@
+package cho.restapi.restapiedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cho/restapi/restapiedu/RestapiEduApplicationTests.java
@@ -0,0 +1,13 @@
+package cho.restapi.restapiedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
